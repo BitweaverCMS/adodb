787459e8a93110d38f60f9247375d543bdbb337e
Max Kremmel <xing@synapse.plus.com>
2006-03-22 10:24:38 +0000

    dos2unix to avoid EOL problems on win

--- pear/Auth/Container/ADOdb.php
+++ pear/Auth/Container/ADOdb.php
@@ -17,7 +17,7 @@
 // |	Richard Tango-Lowy <richtl@arscognita.com>                                  |
 // +----------------------------------------------------------------------+
 //
-// $Id: ADOdb.php,v 1.2 2005/10/12 15:14:12 spiderr Exp $
+// $Id: ADOdb.php,v 1.3 2006/03/22 10:24:34 squareing Exp $
 //
 
 require_once 'Auth/Container.php';
@@ -35,7 +35,7 @@ require_once 'adodb-errorpear.inc.php';
  *
  * @author   Richard Tango-Lowy <richtl@arscognita.com>
  * @package  Auth
- * @version  $Revision: 1.2 $
+ * @version  $Revision: 1.3 $
  */
 class Auth_Container_ADOdb extends Auth_Container
 {
