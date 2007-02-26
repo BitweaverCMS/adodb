324f227b0105ef3de1a5d3e5339d8b261a4dce3c
Lester Caine <lester@lsces.co.uk>
2007-02-26 15:09:24 +0000

    Tidy up generator stuff using the FB1.5 and FB2 functions

--- drivers/adodb-ibase.inc.php
+++ drivers/adodb-ibase.inc.php
@@ -258,15 +258,15 @@ class ADODB_ibase extends ADOConnection {
 	
 	function CreateSequence($seqname,$startID=1)
 	{
-		$ok = $this->Execute(("INSERT INTO RDB\$GENERATORS (RDB\$GENERATOR_NAME) VALUES (UPPER('$seqname'))" ));
+		$ok = $this->Execute(("CREATE GENERATOR $seqname" ));
 		if (!$ok) return false;
-		return $this->Execute("SET GENERATOR $seqname TO ".($startID-1).';');
+		return $this->Execute("SET GENERATOR $seqname TO ".($startID-1));
 	}
 	
 	function DropSequence($seqname)
 	{
 		$seqname = strtoupper($seqname);
-		$this->Execute("delete from RDB\$GENERATORS where RDB\$GENERATOR_NAME='$seqname'");
+		$this->Execute("DROP GENERATOR $seqname");
 	}
 	
 	function GenID($seqname='adodbseq',$startID=1)
@@ -274,7 +274,7 @@ class ADODB_ibase extends ADOConnection {
 		$getnext = ("SELECT Gen_ID($seqname,1) FROM RDB\$DATABASE");
 		$rs = @$this->Execute($getnext);
 		if (!$rs) {
-			$this->Execute(("INSERT INTO RDB\$GENERATORS (RDB\$GENERATOR_NAME) VALUES (UPPER('$seqname'))" ));
+			$this->Execute(("CREATE GENERATOR $seqname" ));
 			$this->Execute("SET GENERATOR $seqname TO ".($startID-1).';');
 			$rs = $this->Execute($getnext);
 		}
